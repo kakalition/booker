68a6e388a5c735f396ecaf1ad16020779800c88b
Kaka <kakalition@gmail.com>
2022-07-12 20:00:08 +0700

    Modify Book migration

--- backend/database/migrations/2022_07_12_124847_create_books_table.php
+++ backend/database/migrations/2022_07_12_124847_create_books_table.php
@@ -6,26 +6,35 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('books', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('books', function (Blueprint $table) {
+      $table->id();
+      $table->string('isbn');
+      $table->string('title');
+      $table->foreignId('genre_id')
+        ->references('id')
+        ->on('genres')
+        ->cascadeOnDelete();
+      $table->integer('total_copies_owned');
+      $table->integer('total_copies_available');
+      $table->timestamp('published_at');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('books');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('books');
+  }
 };
