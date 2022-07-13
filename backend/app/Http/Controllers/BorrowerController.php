2a17d9121d5f29ff3ff67080ed80f5c8595fdb21
Kaka <kakalition@gmail.com>
2022-07-13 17:54:53 +0700

    Create necessary files; Modify Borrowe model and migrations

---
+++ backend/app/Http/Controllers/BorrowerController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Borrower;
+use Illuminate\Http\Request;
+
+class BorrowerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Borrower  $borrower
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Borrower $borrower)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Borrower  $borrower
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Borrower $borrower)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Borrower  $borrower
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Borrower $borrower)
+    {
+        //
+    }
+}
---
+++ backend/database/migrations/2022_07_13_104928_create_borrowers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('borrowers', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('visitor_id')
+        ->constrained('visitors')
+        ->cascadeOnDelete();
+      $table->foreignId('book_id')
+        ->constrained('books')
+        ->cascadeOnDelete();
+      $table->timestamp('end_date');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('borrowers');
+  }
+};
