e1fb282154b8604979cda8810831ce7cbd478c5a
Kaka <kakalition@gmail.com>
2022-07-13 17:58:38 +0700

    Create StoreBorrowerRequest

--- backend/app/Http/Controllers/BorrowerController.php
+++ backend/app/Http/Controllers/BorrowerController.php
@@ -7,58 +7,28 @@ use Illuminate\Http\Request;
 
 class BorrowerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  public function index()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Borrower  $borrower
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Borrower $borrower)
-    {
-        //
-    }
+  public function show(Borrower $borrower)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Borrower  $borrower
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Borrower $borrower)
-    {
-        //
-    }
+  public function update(Request $request, Borrower $borrower)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Borrower  $borrower
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Borrower $borrower)
-    {
-        //
-    }
+  public function destroy(Borrower $borrower)
+  {
+    //
+  }
 }
