bab459a3759c0535b87b6625410d11be409f081d
Kaka <kakalition@gmail.com>
2022-07-14 09:10:50 +0700

    Finish up Author controller and service; Create Store and Update request of Author

--- backend/app/Http/Controllers/AuthorController.php
+++ backend/app/Http/Controllers/AuthorController.php
@@ -2,63 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreAuthorRequest;
+use App\Http\Requests\UpdateAuthorRequest;
 use App\Models\Author;
-use Illuminate\Http\Request;
+use App\Services\AuthorService;
+use Exception;
 
 class AuthorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+  public function index(AuthorService $service)
+  {
+    try {
+      $authors = $service->fetchAll();
+    } catch (Exception $exception) {
+      return response($exception->getMessage(), 500);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    return response($authors, 200);
+  }
+
+  public function store(StoreAuthorRequest $request, AuthorService $service)
+  {
+    $validatedData = $request->validated();
+
+    try {
+      $authors = $service->store($validatedData);
+    } catch (Exception $exception) {
+      return response($exception->getMessage(), 500);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Author $author)
-    {
-        //
+    return response($authors, 201);
+  }
+
+  public function show(Author $author)
+  {
+    //
+  }
+
+  public function update(UpdateAuthorRequest $request, Author $author, AuthorService $service)
+  {
+    $validatedData = $request->validated();
+
+    try {
+      $authors = $service->update($author, $validatedData);
+    } catch (Exception $exception) {
+      return response($exception->getMessage(), 500);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Author $author)
-    {
-        //
+    return response($authors, 200);
+  }
+
+  public function destroy(Author $author, AuthorService $service)
+  {
+    try {
+      $authors = $service->delete($author);
+    } catch (Exception $exception) {
+      return response($exception->getMessage(), 500);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Author $author)
-    {
-        //
-    }
+    return response('', 204);
+  }
 }
