b73620fbc8e4dd1906dfd42442332428f1e97131
Kaka <kakalition@gmail.com>
2022-07-16 12:29:26 +0700

    Checkpoint data holder

--- frontend/src/Features/ManageEntity/Author/ManageAuthorViewModel.tsx
+++ frontend/src/Features/ManageEntity/Author/ManageAuthorViewModel.tsx
@@ -1,20 +1,58 @@
-import {
-  Checkbox, IconButton, Td, Tr,
-} from '@chakra-ui/react';
-import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
 import { AxiosResponse } from 'axios';
+import {
+  map, sort, slice, range,
+} from 'ramda';
 import { useEffect, useMemo, useState } from 'react';
 import AuthorAPI from '../../../API/AuthorAPI';
 import EntityMapper from '../../../Functions/Mappers/EntityMapper';
 import ManageAuthorMapper from '../../../Functions/Mappers/ManageAuthorMapper';
 import AuthorEntity from '../../../Types/Entities/AuthorEntity';
 
-export default function useManageAuthorViewModel() {
+function useAuthorsDataHolder() {
   const [authorsData, setAuthorsData] = useState<AuthorEntity[]>([]);
+  const [preparedData, setPreparedData] = useState<AuthorEntity[]>([]);
+
+  const showsPerPage = 10;
+
+  // Create filter pipeline
+  const applySortBy = null;
+  const applySortOrder = null;
+  const applyPage = (page: number) => {
+    const offset = (showsPerPage * page) - showsPerPage - 1;
+    const sliced = slice(offset, showsPerPage, authorsData);
+    setPreparedData(sliced);
+  };
+
+  const totalPage = preparedData.length / showsPerPage;
+
+  const setData = (entities: AuthorEntity[]) => {
+    setAuthorsData(entities);
+    setPreparedData(entities);
+  };
+
+  return {
+    authorsData: preparedData,
+    setData,
+    applySortBy,
+    applySortOrder,
+    applyPage,
+    totalPage,
+  };
+}
+
+const sortName = (a: AuthorEntity, b: AuthorEntity) => {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+};
+
+export default function useManageAuthorViewModel() {
+  const dataHolder = useAuthorsDataHolder();
 
   const onFetchSuccess = (response: AxiosResponse) => {
-    const data = response.data.map((element: any) => EntityMapper.author(element));
-    setAuthorsData(data);
+    const data = map(EntityMapper.author, response.data);
+    const sorted = sort(sortName, data);
+    dataHolder.setData(sorted);
   };
 
   const fetchAuthors = () => AuthorAPI
@@ -24,8 +62,8 @@ export default function useManageAuthorViewModel() {
   useEffect(() => { fetchAuthors(); }, []);
 
   const authorsElement = useMemo(
-    () => authorsData.map(ManageAuthorMapper.curriedTableRow),
-    [authorsData],
+    () => dataHolder.authorsData.map(ManageAuthorMapper.curriedTableRow),
+    [dataHolder.authorsData],
   );
 
   const sortByElement = useMemo(() => (
@@ -38,12 +76,14 @@ export default function useManageAuthorViewModel() {
     </>
   ), []);
 
-  const pageElement = useMemo(() => (
-    <>
-      <option value="1">1</option>
-      <option value="2">2</option>
-    </>
-  ), []);
+  const pageElement = useMemo(() => {
+    const intRange = range(1, dataHolder.totalPage);
+    const element = intRange.map((value) => (
+      <option value={value}>{value}</option>
+    ));
+
+    return element;
+  }, [dataHolder.totalPage]);
 
   return {
     curriedAuthorsElement: authorsElement,
---
+++ frontend/src/Functions/Interfaces/BiPredicate.ts
@@ -0,0 +1,3 @@
+type BiPredicate<T> = (a: T, b: T) => boolean;
+
+export default BiPredicate;
