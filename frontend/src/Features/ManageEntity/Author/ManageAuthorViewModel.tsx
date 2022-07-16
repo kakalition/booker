56ac4caafab42784f0d37dae75a294a9bb0b289b
Kaka <kakalition@gmail.com>
2022-07-16 11:44:44 +0700

    Moves mapping logic of author row to ManageAuthorMapper

--- frontend/src/Features/ManageEntity/Author/ManageAuthorViewModel.tsx
+++ frontend/src/Features/ManageEntity/Author/ManageAuthorViewModel.tsx
@@ -6,6 +6,7 @@ import { AxiosResponse } from 'axios';
 import { useEffect, useMemo, useState } from 'react';
 import AuthorAPI from '../../../API/AuthorAPI';
 import EntityMapper from '../../../Functions/Mappers/EntityMapper';
+import ManageAuthorMapper from '../../../Functions/Mappers/ManageAuthorMapper';
 import AuthorEntity from '../../../Types/Entities/AuthorEntity';
 
 export default function useManageAuthorViewModel() {
@@ -17,46 +18,13 @@ export default function useManageAuthorViewModel() {
   };
 
   const fetchAuthors = () => AuthorAPI
-    .get({
-      'shows-per-page': (document.querySelector('#pagination-total') as HTMLSelectElement)?.value,
-      page: (document.querySelector('#pagination-page') as HTMLSelectElement)?.value,
-      query: (document.querySelector('#query') as HTMLSelectElement)?.value,
-      'sort-by': (document.querySelector('#sort-by') as HTMLSelectElement)?.value,
-      'sort-order': (document.querySelector('#sort-order') as HTMLSelectElement)?.value,
-    })
+    .get()
     .then(onFetchSuccess, console.log);
 
   useEffect(() => { fetchAuthors(); }, []);
 
   const authorsElement = useMemo(
-    () => authorsData.map(
-      (element, index) => function (
-        onEditClick: (id: number) => void,
-        onDeleteClick: (id: number) => void,
-      ) {
-        return (
-          <Tr key={element.id}>
-            <Td>{index + 1}</Td>
-            <Td>{element.name}</Td>
-            <Td>{element.birth_date}</Td>
-            <Td>{element.total_books}</Td>
-            <Td>{element.total_copies_owned}</Td>
-            <Td>{element.currently_borrowed}</Td>
-            <Td>
-              <div className="flex flex-row items-center gap-6">
-                <IconButton bgColor="transparent" aria-label="edit author" onClick={() => onEditClick(element.id)}>
-                  <PencilIcon className="h-6 w-6 stroke-gray-500" />
-                </IconButton>
-                <IconButton bgColor="transparent" aria-label="delete author" onClick={() => onDeleteClick(element.id)}>
-                  <TrashIcon className="h-6 w-6 stroke-gray-500" />
-                </IconButton>
-                <Checkbox size="lg" />
-              </div>
-            </Td>
-          </Tr>
-        );
-      },
-    ),
+    () => authorsData.map(ManageAuthorMapper.curriedTableRow),
     [authorsData],
   );
 
---
+++ frontend/src/Functions/Mappers/ManageAuthorMapper.tsx
@@ -0,0 +1,41 @@
+import {
+  Checkbox, IconButton, Td, Tr,
+} from '@chakra-ui/react';
+import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
+import AuthorEntity from '../../Types/Entities/AuthorEntity';
+
+namespace ManageAuthorMapper {
+  const tableRow = (
+    element: AuthorEntity,
+    index: number,
+    onEditClick: (id: number) => void,
+    onDeleteClick: (id: number) => void,
+  ) => (
+    <Tr key={element.id}>
+      <Td>{index + 1}</Td>
+      <Td>{element.name}</Td>
+      <Td>{element.birth_date}</Td>
+      <Td>{element.total_books}</Td>
+      <Td>{element.total_copies_owned}</Td>
+      <Td>{element.currently_borrowed}</Td>
+      <Td>
+        <div className="flex flex-row items-center gap-6">
+          <IconButton bgColor="transparent" aria-label="edit author" onClick={() => onEditClick(element.id)}>
+            <PencilIcon className="h-6 w-6 stroke-gray-500" />
+          </IconButton>
+          <IconButton bgColor="transparent" aria-label="delete author" onClick={() => onDeleteClick(element.id)}>
+            <TrashIcon className="h-6 w-6 stroke-gray-500" />
+          </IconButton>
+          <Checkbox size="lg" />
+        </div>
+      </Td>
+    </Tr>
+  );
+
+  export const curriedTableRow = (element: AuthorEntity, index: number) => (
+    onEditClick: (id: number) => void,
+    onDeleteClick: (id: number) => void,
+  ) => tableRow(element, index, onEditClick, onDeleteClick);
+}
+
+export default ManageAuthorMapper;
