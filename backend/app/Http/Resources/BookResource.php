1f66a1d89b692d7651cd8ea3f5315382d745f551
Kaka <kakalition@gmail.com>
2022-07-18 08:08:09 +0700

    Create BookResource

---
+++ backend/app/Http/Resources/BookResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+   */
+  public function toArray($request)
+  {
+    return [
+      'id' => $this->id,
+      'isbn' => $this->isbn,
+      'title' => $this->title,
+      'author_name' => null,
+      'publisher_name' => null,
+      'total_available_copies' => null,
+      'total_copies_owned' => $this->total_copies_owned,
+      'published_at' => $this->published_at,
+    ];
+  }
+}
